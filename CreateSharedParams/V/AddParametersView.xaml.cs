7de916c357e4034472aab7dca2ec4841115195be
Andrew Kozhevnikov <andrew.kozhevnikov93@gmail.com>
2019-08-28 14:46:21 +0300

    added title

--- CreateSharedParams/V/AddParametersView.xaml.cs
+++ CreateSharedParams/V/AddParametersView.xaml.cs
@@ -9,7 +9,12 @@
 
 namespace CreateParams.V
 {
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
     using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
 
     using AddFamilyParameters.V;
 
@@ -36,10 +41,33 @@ namespace CreateParams.V
         public AddParametersView(Document doc)
         {
             this.InitializeComponent();
+            Image img = Properties.Resources.icons8_add_property_32;
+            this.Icon = this.GetImageSource(img);
             this.revitDocument = doc;
             this.viewModel = new AddParametersViewModel(doc);
             
             this.FilePathTextBox.Text = this.viewModel.SharedParametersFilePath;
+        }
+
+        private BitmapImage GetImageSource(Image img)
+        {
+            BitmapImage bmp = new BitmapImage();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, ImageFormat.Png);
+                ms.Position = 0;
+
+                bmp.BeginInit();
+
+                bmp.CacheOption = BitmapCacheOption.OnLoad;
+                bmp.UriSource = null;
+                bmp.StreamSource = ms;
+
+                bmp.EndInit();
+            }
+
+            return bmp;
         }
 
         private void ButtonBrowseSharedParameterFile_OnClick(object sender, RoutedEventArgs e)
